30f44b8af4295b45e6bebf5f7b96af4bbef8efa1
JDing0704 <75730691+JDing0704@users.noreply.github.com>
2020-12-15 09:23:31 +0000

    Update fires.cs

--- Fires/fires.cs
+++ Fires/fires.cs
@@ -3,14 +3,83 @@ namespace trees
 	static class Fires
 	{
 		// Entry point to fires code,
-		/**
-		 * REMEMBER
-		 * 
-		 * Our trees are firebroken in blocks of the nearest 200,#
-		 * so only set fire to trees in blocks, rather than the whole forest
-		 */
-		public static void fires(ref uint[] trees) {
+		
+		
+		public static void fires(ref uint[] trees, ref int wind) 
+		{
 			// Josh, you're up
+			// this calculates which trees will catch fire and sets them to damaged
+			// trees have a 15% chance to survive the fire
+			// the first loop runs to set the initial trees on fire
+			// the second loop then runs to spread the fire depending on wind direction
+			bool new_fire = false;
+			int x = 0;
+			int y = 0;
+			for (int i = 0; i < 10000; i++)
+			{
+				if (tree[x, y].fire != true)
+				{
+					Random randint = new Random();
+					int rand = randint.Next(1, 1000000);
+					if (rand == 1)
+					{
+						tree[x, y].fire = true;
+						new_fire = true;
+					}
+				    	if (x == 100)
+				    	{
+						y++;
+						x = 0;
+					}
+				}
+			}
+
+			x = 0;
+			y = 0;
+
+			while (new_fire == true)
+			{
+				new_fire = false;
+				for (int i = 0; i < 10000; i++)
+				{
+					if (tree[x, y].fire == true)
+					{
+						new_fire = true;
+						if (wind == 0)
+						{
+							tree[x + 1, y].fire = true;
+							tree[x, y + 1].fire = true;
+							tree[x + 1, y + 1].fire = true;
+						}
+						if (wind == 1)
+						{
+							tree[x + 1, y].fire = true;
+							tree[x + 1, y - 1].fire = true;
+							tree[x, y - 1].fire = true;
+						}
+						if (wind = 2)
+						{
+							tree[x, y - 1].fire = true;
+							tree[x - 1, y - 1].fire = true;
+							tree[x, y - 1].fire = true;
+						}
+						if (wind = 3)
+						{
+							tree[x - 1, y].fire = true;
+							tree[x - 1, y + 1].fire = true;
+							tree[x, y + 1].fire = true;
+						}
+					}
+				    	Random randint = new Random();
+				    	int rand = randint.Next(15, 100);
+				    	if (rand >= 15)
+				    	{
+						tree[x, y].damaged = true;
+				    	}
+				    		tree[x, y].fire = false;
+					}
+				}
+			}
 		}
 	}
 }
